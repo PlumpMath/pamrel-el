c0c18209fee74bf3f4468a3bc15bd92ad0872273
Christopher Allan Webber <cwebber@dustycloud.org>
2014-03-15 18:16:04 -0500

    Auto-syntax highlight based on mode

--- pamrel.el
+++ pamrel.el
@@ -25,17 +25,39 @@
 (defvar pamrel-post-url "http://pamrel.lu/")
 (defvar pamrel-confirm-before-post t)
 
+(defvar pamrel-mode-language-map
+  '((emacs-lisp-mode . "scheme")
+    (python-mode . "python")
+    (hy-mode . "hy")
+    (conf-mode . "ini")
+    (html-mode . "css+jinja")
+    (web-mode . "css+jinja")))
+
+(defun pamrel-detect-language-from-major-mode ()
+  "Get the current language by checking the pamrel-mode-language-map"
+  (cdr (assoc major-mode pamrel-mode-language-map)))
+
+; If nil, none is used
+(defvar pamrel-detect-language-method 'pamrel-detect-language-from-major-mode)
+
+(defun pamrel-detect-language-meta-edition ()
+  "Get or don't get the language, depending on if `pamrel-detect-language-method' is set"
+  (if pamrel-detect-language-method
+      (apply pamrel-detect-language-method nil)))
+
 ;; TODO: Add language determination based on buffer
 ;;       eg &language=python
 
-(defun pamrel-post (content)
+(defun pamrel-post (content &optional language)
   "Post CONTENT to pamrel pastebin"
   (let ((url-request-method "POST")
         (url-request-extra-headers
          '(("Content-Type" . "application/x-www-form-urlencoded")))
         (url-request-data
          (concat "content="
-                  (url-hexify-string content))))
+                 (url-hexify-string content)
+                 (if language
+                     (concat "&language=" (url-hexify-string language))))))
     (url-retrieve pamrel-post-url (lambda (status) (switch-to-buffer (current-buffer))))))
 
 (defun pamrel-post-current-buffer ()
@@ -43,11 +65,12 @@
   (interactive)
   (if (or (not pamrel-confirm-before-post)
           (y-or-n-p "Really post the entire buffer to pamrel?"))
-      (pamrel-post (buffer-string))))
+      (pamrel-post (buffer-string) (pamrel-detect-language-meta-edition))))
 
 (defun pamrel-post-region (text-start text-end)
   "Post the contents of this region to pamrel"
   (interactive "r")
   (if (or (not pamrel-confirm-before-post)
           (y-or-n-p "Really post region to pamrel?"))
-      (pamrel-post (buffer-substring text-start text-end))))
+      (pamrel-post (buffer-substring text-start text-end)
+                   (pamrel-detect-language-meta-edition))))
